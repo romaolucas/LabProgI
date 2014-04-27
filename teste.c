8af5643aad50256a045bd22f54ac006c71c1d188
Romao <luck.romao@gmail.com>
2014-04-27 20:59:45 -0300

    Arrumado o memory leak

--- teste.c
+++ teste.c
@@ -7,16 +7,17 @@
 
 int main() {
    int i, k = 1;
-   Point *p, *q;
+   Point *p, *q, *r;
    Defense *d;
    Tiro *t;
    initShip();
    initCenario();
    for (i = 0; i < 10; i++, k *= 7) {
       p = initPoint(k*1.0, k*1.0, k*1.0);
+      r = initPoint(k*1.0, k*1.0, k*1.0);
       q = initPoint(k*2.0, k*2.0, k*2.0);
       d = initDefense(p, 0, NORMAL);
-      t = initTiro(q, p, NORMAL);
+      t = initTiro(q, r, NORMAL);
       includeDefense(d);
       includeTiro(t);
    }
