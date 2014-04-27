6447f43c6c328a7b338b5b11488e26b8113893c9
Cesar <cecerhcp0@hotmail.com>
2014-04-27 17:27:47 -0300

    funcao naveShoot() (alguns valores temporarios)

--- Nave.c
+++ Nave.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "Nave.h"
 #include "Tiro.h"
+#include "Cenario.h"
 #include "general.h"
 
 Ship *ship;
@@ -13,13 +14,21 @@ void initShip()
     ship->height = INITIAL_HEIGHT;
     ship->scenarioPos = INITIAL_SPOS;
     ship->horizontalPos = INITIAL_HPOS;
-    ship->orientation = initPoint(0, 0, 0);/*direcao do começo*/
+    ship->orientation = initPoint(0, 0, 0);/*direcao do comeï¿½o*/
     ship->attackType = NORMAL;
 }
 
 int isShipDestroyed()
 {
    return ship->hp <= 0;
+}
+
+void naveShoot()
+{  
+   Point *position = initPoint(1,1,1);/* valor tem que ser mudado para a posicao onde o tiro vai nascer */
+   Point *orientation = initPoint(ship->orientation->x, ship->orientation->y, ship->orientation->z);
+   Tiro *tiro = initTiro(position, orientation, INITIAL_SPEED, 10, NORMAL);
+   includeTiro(tiro);
 }
 
 void shipGotHit(int dmg)
